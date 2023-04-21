9a0f74de25a3e4cb3c268717b1cadc0b85b22080
OE-Heart <leoouyixin@126.com>
2023-04-21 12:55:24 +0800

    feat: release version 0.0.3

--- easyinstruct/utils/api.py
+++ easyinstruct/utils/api.py
@@ -8,7 +8,7 @@ API_NAME_DICT = {
     "google": {},
     "baidu": {},
     "anthropic": {
-        "claude": ["claude-v1", "claude-v1.0", "claude-v1.2"],
+        "claude": ["claude-v1", "claude-v1.0", "claude-v1.2", "claude-v1.3"],
         "claude-instant": ["claude-instant-v1", "claude-instant-v1.0"]
     }
 }
