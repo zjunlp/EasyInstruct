222e82903e2778c543f0546c619d725ecc4ff161
OE-Heart <leoouyixin@126.com>
2023-10-08 16:59:37 +0800

    feat: add gpt_score_selector

--- easyinstruct/selectors/__init__.py
+++ easyinstruct/selectors/__init__.py
@@ -2,4 +2,5 @@ from .base_selector import BaseSelector
 from .length_selector import LengthSelector
 from .deduplicator import Deduplicator
 from .rouge_score_selector import RougeScoreSelector
+from .gpt_score_selector import GPTScoreSelector
 from .compositional_selector import CompositionalSelector
--- examples/selector.py
+++ examples/selector.py
@@ -1,7 +1,12 @@
-from easyinstruct import LengthSelector, Deduplicator, RougeScoreSelector, CompositionalSelector
+from easyinstruct.utils.api import set_openai_key, set_proxy
+from easyinstruct import LengthSelector, Deduplicator, RougeScoreSelector, CompositionalSelector, GPTScoreSelector
+
+set_openai_key("")
+set_proxy("http://127.0.0.1:7890")
 
 selector1 = Deduplicator()
 selector2 = RougeScoreSelector()
 selector3 = LengthSelector()
-selector = CompositionalSelector(source_file_path="evolved_instances.jsonl", selectors_list=[selector1, selector2, selector3])
+selector4 = GPTScoreSelector()
+selector = CompositionalSelector(source_file_path="evolved_instances.jsonl", selectors_list=[selector4])
 selector.process()
