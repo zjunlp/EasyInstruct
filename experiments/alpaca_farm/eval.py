1ab65073f3a3577206e0d907aff8fb5c2ab72a34
OE-Heart <leoouyixin@126.com>
2024-01-11 23:14:07 +0800

    update alpaca_farm experiments

--- experiments/alpaca_farm/eval.py
+++ experiments/alpaca_farm/eval.py
@@ -1,13 +1,15 @@
 import os
+import json
 import openai
 import argparse
 
 openai.api_key = ''
-os.environ['OPENAI_API_KEY'] = openai.api_key
 from alpaca_farm.auto_annotations.analysis import head2head_to_metrics
-from alpaca_farm.utils import jload
 from alpaca_farm.auto_annotations import PairwiseAutoAnnotator
 
+from easyinstruct.utils.api import set_proxy
+
+set_proxy("http://127.0.0.1:7890")
 
 if 'OPENAI_API_KEY' not in os.environ:
     decoding_kwargs = dict(
@@ -25,12 +27,21 @@ parser.add_argument('--target', type=str)
 
 args = parser.parse_args()
 
-outputs_baseline = jload(args.baseline)
-outputs_target = jload(args.target)
+if args.baseline.endswith(".jsonl"):
+    outputs_baseline = [json.loads(l) for l in open(args.baseline, "r")]
+elif args.baseline.endswith(".json"):
+    outputs_baseline = json.load(open(args.baseline, "r"))
+
+if args.target.endswith(".jsonl"): 
+    outputs_target = [json.loads(l) for l in open(args.target, "r")]
+elif args.target.endswith(".json"):
+    outputs_target = json.load(open(args.target, "r"))
 
 annotator_pool = PairwiseAutoAnnotator(annotators_config="annotators/test/configs.yaml", **decoding_kwargs)
 
 annotated_sft = annotator_pool.annotate_head2head(outputs_1=outputs_baseline, outputs_2=outputs_target)
 res = head2head_to_metrics(preferences=[a["preference"] for a in annotated_sft])
 
+print(f'base: {args.baseline}')
+print(f'target: {args.target}')
 print(res)
---
+++ experiments/alpaca_farm/test.sh
@@ -0,0 +1,3 @@
+python eval.py \
+    --baseline  /zjunlp/oyx/EasyInstruct/experiments/alpaca_farm/generates/llama2-7b-evol_instruct_5k.jsonl \
+    --target /zjunlp/oyx/EasyInstruct/experiments/alpaca_farm/generates/llama2-7b.jsonl
