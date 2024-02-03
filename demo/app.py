15c758daf9da684ceca3790c72d5856e7a697f5d
OE-Heart <leoouyixin@126.com>
2024-02-04 00:36:12 +0800

    Update gradio app demo

--- demo/app.py
+++ demo/app.py
@@ -1,26 +1,45 @@
 import gradio as gr
-import json
 
-from easyinstruct import SelfInstructGenerator, EvolInstructGenerator, BacktranslationGenerator
-from easyinstruct import LengthSelector, Deduplicator, RougeSelector, GPTScoreSelector, MTLDSelector, PPLSelector, RandomSelector, MultiSelector
+from easyinstruct import (
+    SelfInstructGenerator,
+    EvolInstructGenerator,
+    BacktranslationGenerator,
+)
+from easyinstruct import (
+    LengthSelector,
+    Deduplicator,
+    RougeSelector,
+    GPTScoreSelector,
+    MTLDSelector,
+    PPLSelector,
+    RandomSelector,
+    MultiSelector,
+)
 from easyinstruct.utils.api import set_openai_key, set_proxy
 
-set_proxy("http://127.0.0.1:7890")
+set_proxy("")
 
-def generate(seed_data_file_path, openai_api_key, engine, input_generator, num_instructions_to_generate):
+
+def generate(
+    seed_data_file_path,
+    openai_api_key,
+    engine,
+    input_generator,
+    num_instructions_to_generate,
+):
     set_openai_key(openai_api_key)
-    
+
     if input_generator == "Self-Instruct":
         generator = SelfInstructGenerator(
             seed_tasks_path=seed_data_file_path,
             engine=engine,
-            num_instructions_to_generate=num_instructions_to_generate
+            num_instructions_to_generate=num_instructions_to_generate,
         )
     elif input_generator == "Evol-Instruct":
         generator = EvolInstructGenerator(
             seed_tasks_path=seed_data_file_path,
             engine=engine,
-            num_instructions_to_generate=num_instructions_to_generate
+            num_instructions_to_generate=num_instructions_to_generate,
         )
     elif input_generator == "KG2Instruct":
         return "Hello World!"
@@ -28,139 +47,265 @@ def generate(seed_data_file_path, openai_api_key, engine, input_generator, num_i
         generator = BacktranslationGenerator(
             unlabelled_data_path=seed_data_file_path,
             engine=engine,
-            num_instructions_to_generate=num_instructions_to_generate
+            num_instructions_to_generate=num_instructions_to_generate,
         )
     else:
         raise ValueError(f"Unknown generator: {generator}")
-    
+
     generated_data = generator.generate()
-    result_string = ""
-    for data in generated_data:
-        result_string += json.dumps(data, ensure_ascii=False) + "\n"
-    return result_string
-    
-def process(raw_instructions_file_path, openai_api_key, engine, selectors):
-    print(f'raw_instructions_file_path: {raw_instructions_file_path}')
-    print(f'openai_api_key: {openai_api_key}')
-    print(f'engine: {engine}')
-    print(f'selectors: {selectors}')
-    
+    return generated_data
+
+
+def process(
+    raw_instructions_file_path,
+    openai_api_key,
+    engine,
+    selectors,
+    min_instruction_length,
+    max_instruction_length,
+    min_response_length,
+    max_response_length,
+    rouge_threshold,
+    min_mtld,
+    max_mtld,
+    gpt_score_threshold,
+    num_instructions_to_sample,
+):
+    print(f"raw_instructions_file_path: {raw_instructions_file_path}")
+    print(f"openai_api_key: {openai_api_key}")
+    print(f"engine: {engine}")
+    print(f"selectors: {selectors}")
+
     selectors_list = []
     if "Deduplicator" in selectors:
         deduplicator = Deduplicator()
         selectors_list.append(deduplicator)
     if "RougeSelector" in selectors:
-        rouge_selector = RougeSelector(threshold=0.3)
+        rouge_selector = RougeSelector(threshold=rouge_threshold)
         selectors_list.append(rouge_selector)
     if "LengthSelector" in selectors:
-        length_selector = LengthSelector(max_response_length=320)
+        length_selector = LengthSelector(
+            min_instruction_length=min_instruction_length,
+            max_instruction_length=max_instruction_length,
+            min_response_length=min_response_length,
+            max_response_length=max_response_length,
+        )
         selectors_list.append(length_selector)
     if "GPTScoreSelector" in selectors:
         set_openai_key(openai_api_key)
-        gpt_score_selector = GPTScoreSelector(threshold=5, engine=engine)
+        gpt_score_selector = GPTScoreSelector(
+            threshold=gpt_score_threshold, engine=engine
+        )
         selectors_list.append(gpt_score_selector)
     if "MTLDSelector" in selectors:
-        mtld_selector = MTLDSelector()
+        mtld_selector = MTLDSelector(
+            min_mtld=min_mtld,
+            max_mtld=max_mtld,
+        )
         selectors_list.append(mtld_selector)
-    if "PPLSelector" in selectors:
-        ppl_selector = PPLSelector(threshold=50)
-        selectors_list.append(ppl_selector)
+    # if "PPLSelector" in selectors:
+    #     ppl_selector = PPLSelector(threshold=50)
+    #     selectors_list.append(ppl_selector)
     if "RandomSelector" in selectors:
-        random_selector = RandomSelector(num_instructions_to_sample=5000)
+        random_selector = RandomSelector(
+            num_instructions_to_sample=num_instructions_to_sample
+        )
         selectors_list.append(random_selector)
-    
-    selector = MultiSelector(source_file_path=raw_instructions_file_path, selectors_list=selectors_list)
-    
-    selected_data =  selector.process()
-    result_string = ""
-    for data in selected_data:
-        result_string += json.dumps(data, ensure_ascii=False) + "\n"
-    return result_string
-    
+
+    selector = MultiSelector(
+        source_file_path=raw_instructions_file_path, selectors_list=selectors_list
+    )
+
+    selected_data = selector.process()
+    return selected_data
+
+
 with gr.Blocks() as gradio_app:
-    
     ##############
     # Head Block #
     ##############
     with gr.Row(equal_height=True):
-        with gr.Column(scale=5):
-            gr.HTML(
-                """
-                <div align="center">
+        gr.HTML(
+            """
+            <div>
+                <h1>🎨EasyInstruct: An Easy-to-use Instruction Processing Framework for Large Language Models</h1>
+                
+                <p>
+                📍[<a href="https://zjunlp.github.io/project/EasyInstruct" target="_blank">Project Page</a>]
+                📑[<a href="" target="_blank">Paper</a>]
+                👨‍💻[<a href="https://github.com/zjunlp/EasyInstruct" target="_blank"><span class="icon"><i class="fab fa-github"></i></span>Code</a>]
+                🤗[<a href="https://huggingface.co/spaces/zjunlp/EasyInstruct" target="_blank">Demo</a>]
+                </p>
 
-                    <img src="https://raw.githubusercontent.com/zjunlp/EasyInstruct/main/figs/logo.png" width="300px">
-                    
-                    <h3>An Easy-to-use Instruction Processing Framework for Large Language Models.</h3>
+            </div>
+            """
+        )
+    with gr.Row(equal_height=True):
+        openai_api_key = gr.Textbox(label="OpenAI API Key")
+        engine = gr.Dropdown(
+            choices=["gpt-3.5-turbo-instruct", "gpt-3.5-turbo", "gpt-4"],
+            value="gpt-3.5-turbo",
+            label="Engine",
+        )
 
-                    <p align="center">
-                        <a href="">Project</a> •
-                        <a href="">Paper</a> •
-                        <a href="https://github.com/zjunlp/EasyInstruct">Code</a> •
-                        <a href="https://zjunlp.gitbook.io/easyinstruct/">Docs</a> •
-                        <a href="">Demo</a>
-                    </p>
-
-                </div>
-                """
-            )
-        with gr.Column(scale=1):
-            openai_api_key = gr.Textbox(label="OpenAI API Key")
-            engine = gr.Dropdown(choices=["gpt-3.5-turbo-instruct", "gpt-3.5-turbo", "gpt-4"], value="gpt-3.5-turbo", label="Engine")
-            
     ##############
     # Body Block #
     ##############
+    with gr.Row():
+        gr.HTML(
+            """
+            <h2>Instruction Generation</h2>
+            """
+        )
     with gr.Row(equal_height=True):
-        
-        ##############
-        # Left Block #
-        ##############
+        generator = gr.Dropdown(
+            choices=[
+                "Self-Instruct",
+                "Evol-Instruct",
+                "KG2Instruct",
+                "Backtranslation",
+            ],
+            value="Self-Instruct",
+            label="Generators",
+        )
+        num_instructions_to_generate = gr.Slider(
+            minimum=5,
+            maximum=100,
+            value=10,
+            step=5,
+            label="Generation Number",
+        )
+    with gr.Row(equal_height=True):
+        seed_data_file_path = gr.File(
+            label="Seed Data", file_types=["text", ".json", ".jsonl"]
+        )
+        generated_instances = gr.JSON(label="Generated Instances")
+    with gr.Row():
         with gr.Column(scale=1):
-            gr.HTML(
-                """
-                <h2>Instruction Generation</h2>
-                """
-            )
-            with gr.Row(equal_height=True):
-                with gr.Column(scale=1):
-                    generator = gr.Dropdown(choices=["Self-Instruct", "Evol-Instruct", "KG2Instruct", "Backtranslation"], value="Self-Instruct", label="Generators")
-                with gr.Column(scale=1):
-                    num_instructions_to_generate = gr.Slider(minimum=0, maximum=1000, value=10, step=10, label="Generation Number")
-            seed_data_file_path = gr.File(label="Seed Data", file_types=["text", ".json", ".jsonl"])
-            with gr.Row():
-                with gr.Column(scale=1):
-                    submit_button_1 = gr.Button("Generate", variant="primary")
-                with gr.Column(scale=1):
-                    clear_button_1 = gr.ClearButton()
-                    
-            generated_instances = gr.Textbox(label="Generated Instances",show_copy_button=True)
-            
-            submit_button_1.click(generate, inputs=[seed_data_file_path, openai_api_key, engine, generator, num_instructions_to_generate], outputs=generated_instances)
-            clear_button_1.click(lambda: ("", ""), outputs=[seed_data_file_path, generated_instances])
-        
-        ###############
-        # Right Block #
-        ###############
+            submit_button_1 = gr.Button("Generate", variant="primary")
         with gr.Column(scale=1):
-            gr.HTML(
-                """
-                <h2>Instruction Selection</h2>
-                """
-            )
-            with gr.Row(equal_height=True):
-                selectors = gr.CheckboxGroup(choices=["Deduplicator", "RougeSelector", "LengthSelector", "GPTScoreSelector", "MTLDSelector", "PPLSelector", "RandomSelector"], label="Selectors")
-            raw_instructions_file_path = gr.File(label="Raw Instructions", file_types=["text", ".json", ".jsonl"])
-            with gr.Row():
-                with gr.Column(scale=1):
-                    submit_button_2 = gr.Button("Process", variant="primary")
-                with gr.Column(scale=1):
-                    clear_button_2 = gr.ClearButton()
-                    
-            selected_instances = gr.Textbox(label="Selected Instances",show_copy_button=True)
-            
-            submit_button_2.click(process, inputs=[raw_instructions_file_path, openai_api_key, engine, selectors], outputs=selected_instances)
-            clear_button_2.click(lambda: ("", ""), outputs=[raw_instructions_file_path, selected_instances])
-    
+            clear_button_1 = gr.ClearButton()
+
+        submit_button_1.click(
+            generate,
+            inputs=[
+                seed_data_file_path,
+                openai_api_key,
+                engine,
+                generator,
+                num_instructions_to_generate,
+            ],
+            outputs=generated_instances,
+        )
+        clear_button_1.click(
+            lambda: ("", ""), outputs=[seed_data_file_path, generated_instances]
+        )
+
+    with gr.Row(equal_height=True):
+        gr.HTML(
+            """
+            <h2>Instruction Selection</h2>
+            """
+        )
+    with gr.Row():
+        selectors = gr.CheckboxGroup(
+            choices=[
+                "Deduplicator",
+                "RougeSelector",
+                "LengthSelector",
+                "GPTScoreSelector",
+                "MTLDSelector",
+                # "PPLSelector",
+                "RandomSelector",
+            ],
+            label="Selectors",
+        )
+    with gr.Row():
+        with gr.Accordion("Parameters", open=False):
+            with gr.Accordion("Length Selector", open=False):
+                min_instruction_length = gr.Slider(
+                    label="Min Instruction Length",
+                    minimum=1,
+                    maximum=1024,
+                    value=3,
+                    step=1,
+                )
+                max_instruction_length = gr.Slider(
+                    label="Max Instruction Length",
+                    minimum=1,
+                    maximum=1024,
+                    value=150,
+                    step=1,
+                )
+                min_response_length = gr.Slider(
+                    label="Min Response Length", minimum=1, maximum=2048, value=1
+                )
+                max_response_length = gr.Slider(
+                    label="Max Response Length", minimum=1, maximum=2048, value=350
+                )
+            with gr.Accordion("Rouge Selector", open=False):
+                rouge_threshold = gr.Slider(
+                    label="Rouge Threshold",
+                    minimum=0.0,
+                    maximum=1.0,
+                    value=0.7,
+                    step=0.1,
+                )
+            with gr.Accordion("MTLD Selector", open=False):
+                min_mtld = gr.Slider(
+                    label="Min MTLD", minimum=0, maximum=100, value=8, step=1
+                )
+                max_mtld = gr.Slider(
+                    label="Max MTLD", minimum=0, maximum=100, value=22, step=1
+                )
+
+            with gr.Accordion("GPT Score Selector", open=False):
+                gpt_score_threshold = gr.Slider(
+                    label="GPT Score Threshold", minimum=1, maximum=5, value=4, step=1
+                )
+            with gr.Accordion("Random Selector", open=False):
+                num_instructions_to_sample = gr.Slider(
+                    label="Number of Instructions to Sample",
+                    minimum=5,
+                    maximum=1000,
+                    value=50,
+                    step=5,
+                )
+    with gr.Row(equal_height=True):
+        raw_instructions_file_path = gr.File(
+            label="Raw Instructions", file_types=["text", ".json", ".jsonl"]
+        )
+        selected_instances = gr.JSON(label="Selected Instances")
+    with gr.Row():
+        with gr.Column(scale=1):
+            submit_button_2 = gr.Button("Process", variant="primary")
+        with gr.Column(scale=1):
+            clear_button_2 = gr.ClearButton()
+
+        submit_button_2.click(
+            process,
+            inputs=[
+                raw_instructions_file_path,
+                openai_api_key,
+                engine,
+                selectors,
+                min_instruction_length,
+                max_instruction_length,
+                min_response_length,
+                max_response_length,
+                rouge_threshold,
+                min_mtld,
+                max_mtld,
+                gpt_score_threshold,
+                num_instructions_to_sample,
+            ],
+            outputs=selected_instances,
+        )
+        clear_button_2.click(
+            lambda: ("", ""),
+            outputs=[raw_instructions_file_path, selected_instances],
+        )
+
     ##############
     # Foot Block #
     ##############
@@ -173,22 +318,6 @@ with gr.Blocks() as gradio_app:
               title = {EasyInstruct: An Easy-to-use Instruction Processing Framework for Large Language Models},
               year = {2023},
               url = {https://github.com/zjunlp/EasyInstruct},
-            }
-
-            @misc{knowlm,
-              author = {Ningyu Zhang and Jintian Zhang and Xiaohan Wang and Honghao Gui and Kangwei Liu and Yinuo Jiang and Xiang Chen and Shengyu Mao and Shuofei Qiao and Yuqi Zhu and Zhen Bi and Jing Chen and Xiaozhuan Liang and Yixin Ou and Runnan Fang and             Zekun Xi and Xin Xu and Lei Li and Peng Wang and Mengru Wang and Yunzhi Yao and Bozhong Tian and Yin Fang and Guozhou Zheng and Huajun Chen},
-              title = {KnowLM: An Open-sourced Knowledgeable Large Langugae Model Framework},
-              year = {2023},
-             url = {http://knowlm.zjukg.cn/},
-            }
-
-            @misc{bi2023programofthoughts,
-                  author={Zhen Bi and Ningyu Zhang and Yinuo Jiang and Shumin Deng and Guozhou Zheng and Huajun Chen},
-                  title={When Do Program-of-Thoughts Work for Reasoning?}, 
-                  year={2023},
-                  eprint={2308.15452},
-                  archivePrefix={arXiv},
-                  primaryClass={cs.CL}
             }
             ```
             """
